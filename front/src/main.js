0a24d36df1713e1931ff03037272da3d5a4eb22f
Martin Piazzon <mpiazzon@hotmail.com>
2024-01-18 02:38:57 -0300

    .

--- front/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import router from "./router";
-import { loadFonts } from "./plugins/webfontloader";
-
-loadFonts();
-
-createApp(App).use(router).use(router).use(vuetify).mount("#app");
